9b2bd09c4d25cf0411a2e2c97b217a1774102f58
Arthur Fulconis <arthur.fulconis@edu.escpeurope.eu>
2015-10-29 19:25:08 +0100

    On progress

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,5 +1,5 @@
 class Cocktail < ActiveRecord::Base
-  has_many :ingredients, through: :doses
+  has_many :ingredients, through: :doses # (pour avoir directement les ingrédients d'un cocktail en passant par les doses )
   has_many :doses, dependent: :destroy
 
   validates :name, uniqueness: true, presence: true
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,6 +1,6 @@
 class Ingredient < ActiveRecord::Base
-  has_many :cocktails
-  has_many :doses
+  has_many :cocktails, through: :doses # (si on veut showview pour ingredient : vodka.cocktail ça va nous donner tous les cocktails qui ont vodka, méthode raccourci)
+  has_many :doses, dependent: :destroy
 
   validates :name, uniqueness: true, presence: true
 end
