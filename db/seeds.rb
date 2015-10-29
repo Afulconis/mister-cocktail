1094111ef08bb9a99fb3c3a7aa5ee6e8fbc054ed
Arthur Fulconis <arthur.fulconis@edu.escpeurope.eu>
2015-10-29 18:18:19 +0100

    Reprise

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,26 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
-Ingredient.create(name: "Banana")
-Ingredient.create(name: "Maracuya")
+lemon = Ingredient.create(name: "lemon")
+ice = Ingredient.create(name: "ice")
+mint = Ingredient.create(name: "mint leaves")
+banana = Ingredient.create(name: "Banana")
+maracuja = Ingredient.create(name: "Maracuya")
 
-Cocktail.create(name: "mojito")
-Cocktail.create(name: "Bloody Mary")
+mojito = Cocktail.create(name: "mojito")
+bloody_mary = Cocktail.create(name: "Bloody Mary")
 Cocktail.create(name: "Ginger Ale")
 Cocktail.create(name: "Cuba Libreeeee")
 Cocktail.create(name: "Vodka pure ma gueule")
+
+dose_mint = Dose.new(description: "5 feuilles de menthe")
+dose_mint.ingredient = mint
+dose_mint.cocktail = mojito
+dose_mint.save
+
+mojito.doses.create(description: "5 feuilles de menthe", ingredient_id: mint.id)
+bloody_mary.doses.create(description: "Tomato", ingredient_id: mint.id)
+mojito.doses.create(description: "5 feuilles de menthe", ingredient_id: ice.id)
+mojito.doses.create(description: "5 feuilles de menthe", ingredient_id: mint.id)
+mojito.doses.create(description: "5 feuilles de menthe", ingredient_id: mint.id)
 
