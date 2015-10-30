ee6e9bdc360b5ca26e67475f7674b0885b42dee7
Arthur Fulconis <arthur.fulconis@edu.escpeurope.eu>
2015-10-30 15:56:39 +0100

    configuring Aws

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -3,4 +3,10 @@ class Cocktail < ActiveRecord::Base
   has_many :doses, dependent: :destroy
 
   validates :name, uniqueness: true, presence: true
+
+  has_attached_file :picture,
+    styles: { medium: "300x300>", thumb: "100x100>" }
+
+  validates_attachment_content_type :picture,
+    content_type: /\Aimage\/.*\z/
 end
